e7f72f1df221d88e832a0e39f04a9fb2bcae5bf6
Aadhil Kassim <aadhikassim@gmail.com>
2025-03-14 23:09:20 +0530

    Added a simple balloon animation when age is displayed

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,14 @@ st.title("Welcome to My Streamlit App")
 st.sidebar.header("Navigation")
 st.sidebar.text("Use the sidebar to navigate")
 
-
 st.header("Age Detector")
 st.write("Come and find out your true mental age based on your name!")
-
 
 user_input = st.text_input("Enter a name to guess the age:")
 if user_input:
     guessed_age = get_guessed_age(user_input)
     if guessed_age is not None:
-        st.write(f"The guessed age for the name '{user_input}' is: {guessed_age}")
+        st.markdown(f"<h1 style='text-align: center; color: Blue;'>{guessed_age}</h1>", unsafe_allow_html=True)
+        st.balloons()
     else:
         st.write("Failed to fetch data from the API. Please try again later.")
