715423904776c0ddbd6545431b3b973d57403914
Aadhil Kassim <aadhikassim@gmail.com>
2025-03-16 20:20:55 +0530

    Get joke section moved into functions file.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import requests
+import streamlit as st
 
 def get_guessed_age(name):
     """
@@ -13,3 +14,23 @@ def get_guessed_age(name):
             return None
     except Exception as e:
         return None
+    
+def get_joke(joke_category):
+    """
+    Calls the JokeAPI to get a joke based on the given category.
+    """
+    # Fetch and display a joke based on the selected category
+    if joke_category != " ":
+        st.subheader("Here's a joke to brighten your day:")
+        try:
+            response = requests.get(f"https://sv443.net/jokeapi/v2/joke/{joke_category}")
+            if response.status_code == 200:
+                joke_data = response.json()
+                if joke_data["type"] == joke_category:
+                    st.write(joke_data["joke"])
+                elif joke_data["type"] == "twopart":
+                    st.write(f"{joke_data['setup']} ... {joke_data['delivery']}")
+            else:
+                st.warning("Couldn't fetch a joke at the moment. Please try again later.")
+        except Exception as e:
+            st.error(f"An error occurred while fetching a joke: {e}")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-from functions import get_guessed_age
+from functions import *
 
 st.set_page_config(
    page_title="Mental-ly",
@@ -60,21 +60,7 @@ if st.session_state.page == "Home":
         )
         st.caption("Additional options coming soon!")
 
-        # Fetch and display a joke based on the selected category
-        if joke_category != " ":
-            st.subheader("Here's a joke to brighten your day:")
-            try:
-                response = requests.get(f"https://sv443.net/jokeapi/v2/joke/{joke_category}")
-                if response.status_code == 200:
-                    joke_data = response.json()
-                    if joke_data["type"] == joke_category:
-                        st.write(joke_data["joke"])
-                    elif joke_data["type"] == "twopart":
-                        st.write(f"{joke_data['setup']} ... {joke_data['delivery']}")
-                else:
-                    st.warning("Couldn't fetch a joke at the moment. Please try again later.")
-            except Exception as e:
-                st.error(f"An error occurred while fetching a joke: {e}")
+        X
 
 elif st.session_state.page == "About":
     st.header("About")
